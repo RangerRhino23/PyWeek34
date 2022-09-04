6485848782ce9204dccb06baf81235ed2eaf1158
RangerRhino23 <ryadahlke@outlook.com>
2022-09-04 12:15:00 -0500

    modified:   assets/basetemplate.png 	new file:   assets/ground2.png 	modified:   assets/wall4.png 	modified:   run_game.py

--- run_game.py
+++ run_game.py
@@ -18,17 +18,21 @@ def input(key):
 player = FirstPersonController(speed=20, y=3, position=Vec3(90, -3.5, -90))
 
 #Ground
-ground = Entity(model='cube', scale=(200,1,200), y=-4, texture='assets/ground.png', collider='box')
+ground = Entity(model='cube', scale=(200,1,200), y=-4, texture='assets/ground2.png', collider='box')
 
 def build_base():
     #wall = Entity(model='cube', scale=(5,5,1), position=(10,-1,10), texture='assets/wall.png', collider='box')
-    starter_wall1 = Entity(model='cube', scale=(20,5,1), position=(90,-1,-100), texture='assets/wall4.png', collider='box')
-    starter_wall2 = Entity(model='cube', scale=(1,5,20), position=(100,-1,-90), texture='assets/wall2.png', collider='box')
-    starter_wall3 = Entity(model='cube', scale=(15,5,1), position=(92,-1,-80), texture='assets/wall3.png', collider='box')
-    starter_wall4 = Entity(model='cube', scale=(1,5,20), position=(80,-1,-90), texture='assets/wall.png', collider='box')
-    hallway_spawn_main_wall1 = Entity(model='cube', scale=(1,5,10), position=(85,-1,-74.5), texture='assets/wall.png', collider='box')
-    hallway_spawn_main_wall2 = Entity(model='cube', scale=(1,5,10), position=(80,-1,-74.5), texture='assets/wall2.png', collider='box')
-    test = Entity(model='assets/models/tinker.obj', position=Vec3(93, -3.5, -93))
+    wall1 = Entity(model='cube', scale=(20,5,1), position=(90,-1,-100), texture='assets/wall4.png', collider='box')
+    wall2 = Entity(model='cube', scale=(1,5,20), position=(100,-1,-90), texture='assets/wall2.png', collider='box')
+    wall3 = Entity(model='cube', scale=(15,5,1), position=(92,-1,-80), texture='assets/wall3.png', collider='box')
+    wall4 = Entity(model='cube', scale=(1,5,15), position=(80,-1,-92), texture='assets/wall.png', collider='box')
+    wall5 = Entity(model='cube', scale=(1,5,10), position=(85,-1,-74.5), texture='assets/wall.png', collider='box')
+    wall6 = Entity(model='cube', scale=(1,5,10), position=(80,-1,-74.5), texture='assets/wall2.png', collider='box')
+    wall7 = Entity(model='cube', scale=(10,5,1), position=(74.5,-1,-85), texture='assets/wall3.png', collider='box')
+    wall8 = Entity(model='cube', scale=(11,5,1), position=(75,-1,-80), texture='assets/wall2.png', collider='box')
+    wall9 = Entity(model='cube', scale=(1,5,11), position=(70,-1,-91), texture='assets/wall.png', collider='box')
+    wall10 = Entity(model='cube', scale=(20,5,1), position=(59.5,-1,-97), texture='assets/wall4.png', collider='box')
+    wall12 = Entity(model='cube', scale=(1,5,10), position=(70,-1,-74.5), texture='assets/wall2.png', collider='box')
 build_base()
 
 basetemplate = Entity(model='quad', scale=2, position=(5,-1,5), texture='assets/basetemplate.png')
