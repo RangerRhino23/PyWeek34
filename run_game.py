faaf8f943806456b42c57ff1f1668a222c1e3016
RangerRhino23 <ryadahlke@outlook.com>
2022-09-04 14:49:42 -0500

    new file:   __pycache__/run_game.cpython-310.pyc 	new file:   __pycache__/start_screen.cpython-310.pyc 	new file:   assets/logo.png 	new file:   assets/start_button.png 	modified:   run_game.py 	new file:   start_screen.py

--- run_game.py
+++ run_game.py
@@ -6,7 +6,13 @@ Sky(texture='assets/sky.png')
 
 
 def update():
-    pass
+    camera.y = 0
+    player.speed = 20
+    if held_keys['r']:
+        player.speed = 25
+    if held_keys['left shift']:
+        camera.y = -0.25
+        player.speed = 15
 
 def input(key):
     if key == 'q':
@@ -35,6 +41,15 @@ def build_base():
     wall11 = Entity(model='cube', scale=(1,5,20), position=(50,-1,-86.5), texture='assets/wall2.png', collider='box')
     wall12 = Entity(model='cube', scale=(20,5,1), position=(59.5,-1,-70), texture='assets/wall4.png', collider='box')
     wall13 = Entity(model='cube', scale=(1,5,10), position=(70,-1,-74.5), texture='assets/wall2.png', collider='box')
+    wall14 = Entity(model='cube', scale=(10,5,1), position=(44.5,-1,-77), texture='assets/wall.png', collider='box')
+    wall15 = Entity(model='cube', scale=(10,5,1), position=(44.5,-1,-70), texture='assets/wall3.png', collider='box')
+    wall16 = Entity(model='cube', scale=(1,5,20), position=(40,-1,-87.5), texture='assets/wall2.png', collider='box')
+    wall17 = Entity(model='cube', scale=(20,5,1), position=(29.5,-1,-97), texture='assets/wall4.png', collider='box')
+    wall18 = Entity(model='cube', scale=(1,5,20), position=(19,-1,-87), texture='assets/wall2.png', collider='box')
+    wall19 = Entity(model='cube', scale=(20,5,1), position=(29.5,-1,-59), texture='assets/wall4.png', collider='box')
+    wall20 = Entity(model='cube', scale=(1,5,10), position=(40,-1,-64.5), texture='assets/wall2.png', collider='box')
+    wall24 = Entity(model='cube', scale=(1,5,20), position=(100,-1,-58.5), texture='assets/wall2.png', collider='box')
+    wall25 = Entity(model='cube', scale=(15,5,1), position=(92,-1,-69), texture='assets/wall3.png', collider='box')
 build_base()
 
 basetemplate = Entity(model='quad', scale=2, position=(5,-1,5), texture='assets/basetemplate.png')
---
+++ start_screen.py
@@ -0,0 +1,26 @@
+from fileinput import close
+from ursina import *
+
+app = Ursina()
+
+
+def update():
+    pass
+
+def input(key):
+    if key == 'q':
+        quit()
+
+def run_game():
+    os.startfile('run_game.py')
+    app.closeWindow()
+
+def quit_game():
+    app.closeWindow()
+
+logo = Entity(model='quad', texture='assets/logo.png', scale=(8,4), y = 0.1)
+
+start_button = Button(scale=(.875*.24, .05), x=(logo.x-0.05), y=(logo.y-0.2), text='Start Game', on_click=run_game)
+quit_button = Button(scale=(.875*.24, .05), x=(start_button.x), y=(start_button.y-0.06), text='Quit', on_click=quit_game)
+
+app.run()
