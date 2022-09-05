45d1f51c74827f20e9757ea52f2050b0a0c0404e
RangerRhino23 <ryadahlke@outlook.com>
2022-09-05 09:18:09 -0500

    new file:   assets/models/austronaut.glb 	new file:   assets/models/rover.glb 	modified:   run_game.py

--- run_game.py
+++ run_game.py
@@ -7,12 +7,12 @@ Sky(texture='assets/sky.png')
 
 def update():
     camera.y = 0
-    player.speed = 20
+    player.speed = 15
     if held_keys['r']:
-        player.speed = 25
+        player.speed = 20
     if held_keys['left shift']:
         camera.y = -0.25
-        player.speed = 15
+        player.speed = 10
 
 def input(key):
     if key == 'q':
@@ -23,13 +23,12 @@ def input(key):
         print(player.position)
 
 #Player
-player = FirstPersonController(speed=20, y=3, position=Vec3(90, -3.5, -90))
+player = FirstPersonController(speed=15, y=3, position=Vec3(90, -3.5, -90))
 
 #Ground
 ground = Entity(model='cube', scale=(200,1,200), y=-4, texture='assets/ground.png', collider='box')
 
-def build_base():
-    #wall = Entity(model='cube', scale=(5,5,1), position=(10,-1,10), texture='assets/wall.png', collider='box')
+def build_base_walls():
     wall1 = Entity(model='cube', scale=(20,5,1), position=(90,-1,-100), texture='assets/wall4.png', collider='box')
     wall2 = Entity(model='cube', scale=(1,5,20), position=(100,-1,-90), texture='assets/wall2.png', collider='box')
     wall3 = Entity(model='cube', scale=(15,5,1), position=(92,-1,-80), texture='assets/wall3.png', collider='box')
@@ -54,8 +53,11 @@ def build_base():
     wall23 = Entity(model='cube', scale=(20,5,1), position=(90,-1,-48), texture='assets/wall4.png', collider='box')
     wall24 = Entity(model='cube', scale=(1,5,20), position=(100,-1,-58.5), texture='assets/wall2.png', collider='box')
     wall25 = Entity(model='cube', scale=(15,5,1), position=(92,-1,-69), texture='assets/wall3.png', collider='box')
-build_base()
 
-basetemplate = Entity(model='quad', scale=2, position=(5,-1,5), texture='assets/basetemplate.png')
+def build_base_interior():
+    rover = Entity(model='assets/models/rover.glb', scale=(15), position=(28,-0.5,-83), collider='box')
+
+build_base_walls()
+build_base_interior()
 
 app.run()
