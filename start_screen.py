4ea79b8df217eee519137aea2c21f90586552742
RangerRhino23 <ryadahlke@outlook.com>
2022-09-06 21:43:10 -0500

    modified:   sketch.tldr 	modified:   start_screen.py

--- start_screen.py
+++ start_screen.py
@@ -24,7 +24,7 @@ def options():
 logo = Entity(model='quad', texture='assets/logo.png', scale=(8,4), y = 0.1)
 
 start_button = Button(scale=(.875*.24, .05), x=(logo.x-0.05), y=(logo.y-0.2), text='Start Game', on_click=run_game)
-options_menu = Button(scale=(.875*.24, .05), x=(start_button.x), y=(start_button.y-0.06), text='Quit', on_click=options)
+options_menu = Button(scale=(.875*.24, .05), x=(start_button.x), y=(start_button.y-0.06), text='Options', on_click=options)
 quit_button = Button(scale=(.875*.24, .05), x=(options_menu.x), y=(options_menu.y-0.06), text='Quit', on_click=quit_game)
 
 app.run()
